a15a1d11871d988ed5c0de4c844bf9a01d9b2390
Jody-septiawan <jody.septiawan5@gmail.com>
2022-08-31 15:10:19 +0700

    react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,17 +1,40 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 
 // import necessary object from react-router-dom
+import { Routes, Route, Link } from "react-router-dom";
 
 // import "pages" component here
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Profile from "./pages/Profile";
+import SignIn from "./pages/SignIn";
 
 function App() {
   return (
     // define <BrowserRouter> as parent element
     <>
       <div>
-        {/* Setup navigation here */}
+        <ul>
+          <li>
+            <Link to="/"> Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/profile">Profile</Link>
+          </li>
+          <li>
+            <Link to="/signin">SignIn</Link>
+          </li>
+        </ul>
       </div>
-      {/* code <Routes> and <Route> below, according to path and component */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/signin" element={<SignIn />} />
+      </Routes>
     </>
   );
 }
