1b4afbdd7f65a162aafb719ad4583d76d36d5a31
Jody-septiawan <jody.septiawan5@gmail.com>
2022-09-01 09:20:47 +0700

    Url Params

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Home from "./pages/Home";
 import About from "./pages/About";
 import Profile from "./pages/Profile";
 import SignIn from "./pages/SignIn";
-// import component here
+import DetailUser from "./pages/DetailUser";
 
 function App() {
   return (
@@ -33,7 +33,7 @@ function App() {
         <Route exact path="/about" element={<About />} />
         <Route exact path="/profile" element={<Profile />} />
         <Route exact path="/signin" element={<SignIn />} />
-        {/* define new route */}
+        <Route exact path="/detail-user/:id" element={<DetailUser />} />
       </Routes>
     </Router>
   );
--- src/pages/DetailUser.jsx
+++ src/pages/DetailUser.jsx
@@ -1,16 +1,20 @@
 import { useEffect, useState } from "react";
 import { Container } from "react-bootstrap";
 
-// import necessary hooks from react-router-dom 
+// import necessary hooks from react-router-dom
+import { useParams } from "react-router-dom";
 
 const DetailUser = () => {
   const [data, setData] = useState(null);
-  
+
   // call that hooks here and store to variable
+  const params = useParams();
+
+  console.log(data);
 
   // don't worry about this, we'll cover later
   useEffect(() => {
-    fetch(`https://jsonplaceholder.typicode.com/users/${}`)
+    fetch(`https://jsonplaceholder.typicode.com/users/${params.id}`)
       .then((response) => response.json())
       .then((json) => setData(json));
     return () => {
@@ -20,9 +24,12 @@ const DetailUser = () => {
 
   return (
     // code element inside Container
-    // display information that stored in data 
+    // display information that stored in data
     // console.log first to see what information that you get from fetching
     <Container className="text-center">
+      <p>Name: {data?.name}</p>
+      <p>Username: {data?.username}</p>
+      <p>Email: {data?.email}</p>
     </Container>
   );
 };
